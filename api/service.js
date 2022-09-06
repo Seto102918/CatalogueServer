143edff146a5c8f68ec0a5be288667174767cb50
Seto102918 <elbertshb@gmail.com>
2022-09-06 13:42:39 +0700

    env

--- api/service.js
+++ api/service.js
@@ -10,15 +10,9 @@ var firebase_foto = initializeApp(
 			credential: cert({
 				type: "service_account",
 				project_id: process.env.SERVICE_ACCOUNT_FOTO_PROJECT_ID,
-				private_key_id: process.env.SERVICE_ACCOUNT_FOTO_PRIVATE_KEY_ID,
 				private_key: process.env.SERVICE_ACCOUNT_FOTO_PRIVATE_KEY? process.env.SERVICE_ACCOUNT_FOTO_PRIVATE_KEY.replace(/\\n/gm, "\n")
 				: undefined,
 				client_email: process.env.SERVICE_ACCOUNT_FOTO_CLIENT_EMAIL,
-				client_id: process.env.SERVICE_ACCOUNT_FOTO_CLIENT_ID,
-				auth_uri: "https://accounts.google.com/o/oauth2/auth",
-				token_uri: "https://oauth2.googleapis.com/token",
-				auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-				client_x509_cert_url: process.env.SERVICE_ACCOUNT_FOTO_CLIENT_X509_CERT_URL
 			}),
 			storageBucket: 'gs://fotobridal-83326.appspot.com'
 		}, 
@@ -30,15 +24,9 @@ var firebase_main = initializeApp(
 			credential: cert({
 				type: "service_account",
 				project_id: process.env.SERVICE_ACCOUNT_MAIN_PROJECT_ID,
-				private_key_id: process.env.SERVICE_ACCOUNT_MAIN_PRIVATE_KEY_ID,
 				private_key: process.env.SERVICE_ACCOUNT_MAIN_PRIVATE_KEY? process.env.SERVICE_ACCOUNT_MAIN_PRIVATE_KEY.replace(/\\n/gm, "\n")
 				: undefined,
 				client_email: process.env.SERVICE_ACCOUNT_MAIN_CLIENT_EMAIL,
-				client_id: process.env.SERVICE_ACCOUNT_MAIN_CLIENT_ID,
-				auth_uri: "https://accounts.google.com/o/oauth2/auth",
-				token_uri: "https://oauth2.googleapis.com/token",
-				auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-				client_x509_cert_url: process.env.SERVICE_ACCOUNT_MAIN_CLIENT_X509_CERT_URL
 			}),
 		}, 
 		'pertama' 
